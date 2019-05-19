6eb1061663f975aa0805dc7ff8ae68749a9b8510
azulpintor <azulpintor@gmail.com>
2019-05-19 16:31:04 +0900

    한번테스트

--- git-test/src/main/java/com/example/demo/GitTestApplication.java
+++ git-test/src/main/java/com/example/demo/GitTestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitTestApplication {
 
+	/* 최초당!*/
 	public static void main(String[] args) {
 		SpringApplication.run(GitTestApplication.class, args);
 	}
